670f7f9d7b5ef839aa6f833f1cb70bd9882a0f61
Justin R <jr01@outlook.com>
2026-03-16 11:51:41 -0400

    refactored into a function and added functionality that excludes past wordle words

--- WordleBot_v2.py
+++ WordleBot_v2.py
@@ -2,6 +2,8 @@ import itertools
 import os
 import sys
 import traceback
+import requests
+from bs4 import BeautifulSoup
 
 def _pause_on_error(exc_type, exc_value, exc_tb):
     traceback.print_exception(exc_type, exc_value, exc_tb)
@@ -12,124 +14,42 @@ sys.excepthook = _pause_on_error
 knownLetters = []
 greyLetters = []
 
-
-
 greyLettersv = input('Enter the letters that are grey as one word (ex: abcd): ')
-
 greyLetters = list(greyLettersv)
 
-#print(greyLetters)
+w = ['_', '_', '_', '_', '_']
 
-w1 = w2 = w3 = w4 = w5 = '_'
-
-while True:
-   
-   
-    slotAsk = input('Which slot would you like to input a letter for? (1-5), or if you are done, type "done": ')
-    
-    if slotAsk == 'done':
-        break
-    
-     #code for first slot 
-
-    if slotAsk == '1':
-        ask1 = input('Enter the letter you entered in the first slot: ')
-        if ask1 in greyLetters:
+def slotAsk(slotNum):
+    while True:
+        ask = input(f'Enter the letter you entered in slot {slotNum}: ')
+        if ask in greyLetters:
+            print('Invalid input. Please try again.')
+            continue
+        if len(ask)!=1:
             print('Invalid input. Please try again.')
             continue
 
         gORy = input('Is the letter green or yellow? (g or y) ')
         if gORy == 'g':
-            knownLetters.append(ask1)
-            w1 = ask1
+            knownLetters.append(ask)
+            w[slotNum - 1] = ask
+            break
         elif gORy == 'y':
-            knownLetters.append(ask1)
-            w1 != ask1
+            knownLetters.append(ask)
+            break
         else:
             print('Invalid input. Please try again.')
-            continue
-         
-    #code for second slot 
 
-    if slotAsk == '2':
-        ask2 = input('Enter the letter you entered in the second slot: ')
-        if ask2 in greyLetters:
-            print('Invalid input. Please try again.')
-            continue
+while True:
+    slot_input = input('Which slot would you like to input a letter for? (1-5), or if you are done, type "done": ')
 
-        gORy = input('Is the letter green or yellow? ')
-        if gORy == 'green':
-            knownLetters.append(ask2)
-            w2 = ask2
-        elif gORy == 'yellow':
-            knownLetters.append(ask2)
-            w2 != ask2
-        else:
-            print('Invalid input. Please try again.')
-            continue
-  
-    #code for third slot 
+    if slot_input == 'done':
+        break
 
-    if slotAsk == '3':
-        ask3 = input('Enter the letter you entered in the third slot: ')
-        if ask3 in greyLetters:
-            print('Invalid input. Please try again.')
-            continue
-
-        gORy = input('Is the letter green or yellow? ')
-        if gORy == 'green' and ask3 not in greyLetters:
-            knownLetters.append(ask3)
-            w3 = ask3
-        elif gORy == 'yellow' and ask3 not in greyLetters:
-            knownLetters.append(ask3)
-            w3 != ask3
-        else:
-            print('Invalid input. Please try again.')
-            continue
-   
-    #code for fourth slot 
-
-    if slotAsk == '4':
-        ask4 = input('Enter the letter you entered in the fourth slot: ')
-        if ask4 in greyLetters:
-            print('Invalid input. Please try again.')
-            continue
-
-        gORy = input('Is the letter green or yellow? ')
-        if gORy == 'green' and ask4 not in greyLetters:
-            knownLetters.append(ask4)
-            w4 = ask4
-        elif gORy == 'yellow' and ask4 not in greyLetters:
-            knownLetters.append(ask4)
-            w4 != ask4
-        else:
-            print('Invalid input. Please try again.')
-            continue
-
-    #code for fifth slot 
-
-    if slotAsk == '5':
-        ask5 = input('Enter the letter you entered in the fifth slot: ')
-        if ask5 in greyLetters:
-            print('Invalid input. Please try again.')
-            continue
-        gORy = input('Is the letter green or yellow? ')
-        if gORy == 'green' and ask5 not in greyLetters:
-            knownLetters.append(ask5)
-            w5 = ask5
-        elif gORy == 'yellow' and ask5 not in greyLetters:
-            knownLetters.append(ask5)
-            w5 != ask5
-        else:
-            print('Invalid input. Please try again.')
-            continue
-    word = [w1, w2, w3, w4, w5]
-
-
-
-
-
-
+    if slot_input in ('1', '2', '3', '4', '5'):
+        slotAsk(int(slot_input))
+    else:
+        print('Invalid input. Please enter 1-5 or "done".')
 
 
 # Load the list of valid words from a file
@@ -141,17 +61,29 @@ alphabet = [chr(i) for i in range(97, 123)]  # List of all lowercase letters
 valid_letters = [letter for letter in alphabet if letter not in greyLetters]
 
 # Replace '_' with valid letters
-slots = [valid_letters if letter == '_' else [letter] for letter in word]
+slots = [valid_letters if letter == '_' else [letter] for letter in w]
+
+# Fetch past Wordle answers to exclude
+past_answers = set()
+try:
+    response = requests.get('https://www.rockpapershotgun.com/wordle-past-answers', timeout=10)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    ul = soup.find('ul', class_='inline')
+    if ul:
+        for li in ul.find_all('li'):
+            word = li.get_text(strip=True).lower()
+            if len(word) == 5:
+                past_answers.add(word)
+except Exception:
+    print("Warning: Could not fetch past answers. No words will be excluded.")
 
 # Generate and print all combinations that include all known letters and are valid words
 for combination in itertools.product(*slots):
     combination_str = ''.join(combination)
-    if all(letter in combination_str for letter in knownLetters) and combination_str in valid_words:
+    if all(letter in combination_str for letter in knownLetters) and combination_str in valid_words and combination_str not in past_answers:
         print(combination_str)
 
-print("Known letters:", knownLetters)
+# print("Known letters:", knownLetters)
 
 input("press enter to leave")
 print('Goodbye!')
-
-
